356b23aebc75c903b262559ca5be56f4744c8995
b645 <b645@borqs.com>
2014-02-17 19:18:07 +0800

    Android external i2c tool.(Copy from github/fhunleth/android_external_i2c_tools)

---
+++ i2c-tools/Android.mk
@@ -0,0 +1,64 @@
+ifneq ($(TARGET_SIMULATOR),true)
+
+LOCAL_PATH := $(call my-dir)
+
+# i2cdetect
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+		tools/i2cdetect.c \
+		tools/i2cbusses.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include 
+LOCAL_MODULE := i2cdetect
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+# i2cdump
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+		tools/i2cdump.c \
+		tools/i2cbusses.c \
+		tools/util.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include 
+LOCAL_MODULE := i2cdump
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+# i2cset
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+		tools/i2cset.c \
+		tools/i2cbusses.c \
+		tools/util.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include 
+LOCAL_MODULE := i2cset
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+# i2cget
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+		tools/i2cget.c \
+		tools/i2cbusses.c \
+		tools/util.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include 
+LOCAL_MODULE := i2cget
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+endif
