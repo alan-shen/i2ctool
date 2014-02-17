5c25d742fb14774f0abedf0095d5b2a6c192433e
b645 <b645@borqs.com>
2014-02-17 19:25:21 +0800

    Add a tiny tool i2c-util which also a userspace i2c r/w tool. (http://blog.csdn.net/21cnbao/article/details/7919055)
    Author: Barry Song

---
+++ i2c-util/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)  
+include $(CLEAR_VARS)  
+  
+LOCAL_MODULE_TAGS := optional  
+  
+LOCAL_MODULE := i2c-util  
+  
+LOCAL_SRC_FILES += \  
+    i2c-util.c \  
+  
+include $(BUILD_EXECUTABLE) 
