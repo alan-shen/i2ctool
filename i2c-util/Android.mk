22b5828acc02811fd7ce7813d0a03c5aecb3767f
b645 <b645@borqs.com>
2014-02-18 10:47:31 +0800

    Modify the .mk to build "static linked" binary. And upload the binarys.

--- i2c-util/Android.mk
+++ i2c-util/Android.mk
@@ -6,4 +6,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := i2c-util
 LOCAL_SRC_FILES += i2c-util.c 
 
+# For build static linked exe
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES := libc
+
 include $(BUILD_EXECUTABLE)
